8948c63de6585f8f216be43f7e4a0d3237595b7e
John Sundarraj <johnsundarraj88@gmail.com>
2021-06-08 18:42:17 +0530

    Modified Chef workstation configuration.

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 current_dir = File.dirname(__FILE__)
 local_mode false
-log_level :info
-log_location STDOUT
+config_log_level :error
+config_log_location "/var/log/chef/client-stdout.log"
 node_name "tuto"
 client_key "#{current_dir}/secret/tuto.pem"
 chef_server_url "https://chef-infra-server.sloopstash-dev.internal/organizations/sloopstash"
